06b0cb69fc4cb138796c1e8d12c8bfaef30bef6b
Alexander Bezzubov <bzz@apache.org>
2018-09-18 14:26:25 +0200

    ci: sdk binary download improvements
     - move to 'script' section of CI, so after_failure works
     - usee GITHUB_TOKEN for auth requests to GH API
     - add other arch support, not only linux
    
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- _tools/install-lookout-latest.sh
+++ _tools/install-lookout-latest.sh
@@ -4,23 +4,34 @@
 # Depends on GNU grep
 set -x
 
-#TODO(bzz): check local cached version
+#TODO(bzz): check for local cached version first
 
-curl -s --connect-timeout 5 \
+OS="$(uname | tr '[:upper:]' '[:lower:]')"
+
+oIFS=$IFS IFS=' '
+curl -v ${GITHUB_TOKEN:+'-H' "Authorization: token $GITHUB_TOKEN"} \
+    --connect-timeout 5 \
     --max-time 10 \
     --retry 5 \
     --retry-delay 0 \
     --retry-max-time 40\
     "https://api.github.com/repos/src-d/lookout/releases/latest" \
-  |& tee -a ../lookout-install.log \
+  | tee -a ../lookout-install.log \
   | grep -oP '"browser_download_url": "\K(.*)(?=")' \
-  | grep linux \
+  | grep "${OS}" \
   | wget -qi -
 
-if [[ "${PIPESTATUS[0]}" -ne 0 || "${PIPESTATUS[1]}" -ne 0 || "${PIPESTATUS[2]}" -ne 0 || "${PIPESTATUS[4]}" -ne 0 ]]; then
+if [[ "${PIPESTATUS[0]}" -ne 0 || \
+      "${PIPESTATUS[1]}" -ne 0 || \
+      "${PIPESTATUS[2]}" -ne 0 || \
+      "${PIPESTATUS[3]}" -ne 0 || \
+      "${PIPESTATUS[4]}" -ne 0 ]];
+then
   echo "Unable download latest lookout SDK release" >&2
   exit 2
 fi
+IFS=$oIFS; unset -v oIFS
+# http://mywiki.wooledge.org/BashFAQ/050#I_only_want_to_pass_options_if_the_runtime_data_needs_them
 
 if ! tar -xvzf lookout-sdk_*.tar.gz ; then
   echo "Unable to extract lookout release archive" >&2
