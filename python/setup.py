98d7248ea45e66c310a8669979baa998a8d75359
Maxim Sukharev <maxim@sourced.tech>
2018-11-13 12:32:27 +0100

    bump version
    Signed-off-by: Maxim Sukharev <maxim@sourced.tech>

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ import io
 import os
 from setuptools import setup, find_packages
 
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 README = '../README.md'
 
 description = "SDK for writing lookout analyzers"
