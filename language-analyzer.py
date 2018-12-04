7c22c5cda1b6e4f3a414c20773332d9f1ebb466e
Maxim Sukharev <max@smacker.ru>
2018-12-04 14:52:58 +0100

    Re-export all grpc classes and function in pb package
    It makes the api cleaner.
    See https://github.com/src-d/lookout-sdk/pull/39#issuecomment-442855852
    for details.
    
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- language-analyzer.py
+++ language-analyzer.py
@@ -9,29 +9,24 @@ from concurrent.futures import ThreadPoolExecutor
 
 import time
 import grpc
-
-from lookout.sdk import AnalyzerServicer, add_analyzer_to_server
-from lookout.sdk import service_analyzer_pb2
-from lookout.sdk import service_data_pb2_grpc
-from lookout.sdk import service_data_pb2
-from lookout.sdk.grpc import to_grpc_address, create_channel
+from lookout.sdk import pb
 
 from bblfsh import filter as filter_uast
 
 port_to_listen = 2021
-data_srv_addr = to_grpc_address("ipv4://localhost:10301")
+data_srv_addr = pb.to_grpc_address("ipv4://localhost:10301")
 version = "alpha"
 
 
-class Analyzer(AnalyzerServicer):
+class Analyzer(pb.AnalyzerServicer):
     def NotifyReviewEvent(self, request, context):
         print("got review request {}".format(request))
 
         # client connection to DataServe
-        channel = create_channel(data_srv_addr)
-        stub = service_data_pb2_grpc.DataStub(channel)
+        channel = pb.create_channel(data_srv_addr)
+        stub = pb.DataStub(channel)
         changes = stub.GetChanges(
-            service_data_pb2.ChangesRequest(
+            pb.ChangesRequest(
                 head=request.commit_revision.head,
                 base=request.commit_revision.base,
                 want_contents=False,
@@ -47,11 +42,11 @@ class Analyzer(AnalyzerServicer):
                 change.head.path, change.head.language))
             fns = list(filter_uast(change.head.uast, "//*[@roleFunction]"))
             comments.append(
-                service_analyzer_pb2.Comment(
+                pb.Comment(
                     file=change.head.path,
                     line=0,
                     text="language: {}, functions: {}".format(change.head.language, len(fns))))
-        return service_analyzer_pb2.EventResponse(analyzer_version=version, comments=comments)
+        return pb.EventResponse(analyzer_version=version, comments=comments)
 
     def NotifyPushEvent(self, request, context):
         pass
@@ -59,7 +54,7 @@ class Analyzer(AnalyzerServicer):
 
 def serve():
     server = grpc.server(thread_pool=ThreadPoolExecutor(max_workers=10))
-    add_analyzer_to_server(Analyzer(), server)
+    pb.add_analyzer_to_server(Analyzer(), server)
     server.add_insecure_port("0.0.0.0:{}".format(port_to_listen))
     server.start()
 
