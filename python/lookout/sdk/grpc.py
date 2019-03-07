3dedb727b75f2fc9abbf5a22747070ceb4a4a366
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-03-07 10:35:42 +0100

    Modified create_channel signature in order to accept an optional list of interceptors
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- python/lookout/sdk/grpc.py
+++ python/lookout/sdk/grpc.py
@@ -5,12 +5,16 @@ import grpc
 grpc_max_msg_size = 100 * 1024 * 1024  # 100MB
 
 
-def create_channel(target, options=None):
+def create_channel(target, options=None, interceptors=None):
+    # The list of possible options is available here:
+    # https://grpc.io/grpc/core/group__grpc__arg__keys.html
     options = (options or []) + [
         ("grpc.max_send_message_length", grpc_max_msg_size),
         ("grpc.max_receive_message_length", grpc_max_msg_size),
     ]
-    return grpc.insecure_channel(target, options)
+    interceptors = interceptors or []
+    channel = grpc.insecure_channel(target, options)
+    return grpc.intercept_channel(channel, *interceptors)
 
 
 def to_grpc_address(target):
