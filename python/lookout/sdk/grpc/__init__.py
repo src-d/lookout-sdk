fb430a460ebb9e7de49c158eeb0f78c9c258bc2f
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-03-11 23:51:14 +0100

    Moved common gRPC utilities in lookout.sdk.grpc.utils module
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- python/lookout/sdk/grpc/__init__.py
+++ python/lookout/sdk/grpc/__init__.py
@@ -1,8 +1,13 @@
+from lookout.sdk.grpc.utils import to_grpc_address, create_channel, \
+    create_server
 from lookout.sdk.grpc.interceptors.logger import \
     LogUnaryServerInterceptor, LogStreamServerInterceptor, \
     LogUnaryClientInterceptor, LogStreamClientInterceptor
 
 __all__ = [
+    "to_grpc_address",
+    "create_channel",
+    "create_server",
     "LogUnaryServerInterceptor",
     "LogStreamServerInterceptor",
     "LogUnaryClientInterceptor",
