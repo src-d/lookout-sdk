ff3ca8ac9282aebdb2590418697d4e732128048f
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-03-11 20:13:10 +0100

    Added docstrings for lookout.sdk.grpc module
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- python/lookout/sdk/grpc.py
+++ python/lookout/sdk/grpc.py
@@ -1,12 +1,40 @@
 from urllib.parse import urlparse
 from concurrent.futures import ThreadPoolExecutor
 
+from typing import Optional, Tuple, List, Any, Union
+
 import grpc
 
 grpc_max_msg_size = 100 * 1024 * 1024  # 100MB
 
+ClientInterceptor = Union[
+    grpc.UnaryUnaryClientInterceptor,
+    grpc.UnaryStreamClientInterceptor,
+    grpc.StreamUnaryClientInterceptor,
+    grpc.StreamStreamClientInterceptor,
+]
 
-def create_channel(target, options=None, interceptors=None):
+
+def create_channel(
+        target: str,
+        options: Optional[List[Tuple[str, Any]]] = None,
+        interceptors: Optional[List[ClientInterceptor]] = None,
+) -> grpc.Channel:
+    """Creates a gRPC channel
+
+    The gRPC channel is created with the provided options and intercepts each
+    invocation via the provided interceptors.
+
+    The created channel is configured with the following default options:
+        - "grpc.max_send_message_length": 100MB,
+        - "grpc.max_receive_message_length": 100MB.
+
+    :param target: the server address.
+    :param options: optional list of key-value pairs to configure the channel.
+    :param interceptors: optional list of client interceptors.
+    :returns: a gRPC channel.
+
+    """
     # The list of possible options is available here:
     # https://grpc.io/grpc/core/group__grpc__arg__keys.html
     options = (options or []) + [
@@ -18,7 +46,28 @@ def create_channel(target, options=None, interceptors=None):
     return grpc.intercept_channel(channel, *interceptors)
 
 
-def create_server(max_workers, options=None, interceptors=None):
+def create_server(
+        max_workers: int,
+        options: Optional[List[Tuple[str, Any]]] = None,
+        interceptors: Optional[List[grpc.ServerInterceptor]] = None,
+) -> grpc.Server:
+    """Creates a gRPC server
+
+    The gRPC server is created with the provided options and intercepts each
+    incoming RPCs via the provided interceptors.
+
+    The created server is configured with the following default options:
+        - "grpc.max_send_message_length": 100MB,
+        - "grpc.max_receive_message_length": 100MB.
+
+    :param max_workers: the maximum number of workers to use in the underlying
+        futures.ThreadPoolExecutor to be used by the Server to execute RPC
+        handlers.
+    :param options: optional list of key-value pairs to configure the channel.
+    :param interceptors: optional list of server interceptors.
+    :returns: a gRPC server.
+
+    """
     # The list of possible options is available here:
     # https://grpc.io/grpc/core/group__grpc__arg__keys.html
     options = (options or []) + [
@@ -30,11 +79,13 @@ def create_server(max_workers, options=None, interceptors=None):
                        options=options, interceptors=interceptors)
 
 
-def to_grpc_address(target):
-    """
-    converts a standard gRPC target name to a one that is supported by grpcio.
-    """
+def to_grpc_address(target: str) -> str:
+    """Converts a standard gRPC target to one that is supported by grpcio
 
+    :param target: the server address.
+    :returns: the converted address.
+
+    """
     u = urlparse(target)
     if u.scheme == "dns":
         raise ValueError("dns:// not supported")
